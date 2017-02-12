96a3fe3ffbcd675bbb1d6275205a25618a68acbd
MohamedHamed83 <Mohamed.Hamed@outlook.co.nz>
2017-02-12 16:17:54 +1300

    adding webpack production config

---
+++ config/webpack.prod.js
@@ -0,0 +1,123 @@
+'use strict';
+
+// Modules
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [__dirname].concat(args));
+}
+module.exports = function makeWebpackConfig() {
+
+  var config = {};
+
+  config.entry = {
+    vendors: './src/vendors',
+    app: './src/app/app.js'
+  };
+
+
+  config.output = {
+    path: root('../dist'),
+    publicPath: '/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[name].[hash].js'
+  };
+
+
+  config.devtool = 'source-map';
+
+
+  // Initialize module
+  config.module = {
+    rules: [{
+        test: /\.js$/,
+        loaders: ['ng-annotate-loader', 'babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: [{
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+          ],
+        })
+      }, {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        loader: 'file-loader?name=[path][name].[ext]'
+      }, {
+        test: /\.html$/,
+        loader: 'raw-loader'
+      }
+    ]
+  };
+
+  config.plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendors'],
+      minChunks: Infinity
+    }),
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery',
+      $: 'jquery',
+      jquery: 'jquery'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      test: /\.scss$/i,
+      options: {
+        postcss: {
+          plugins: [autoprefixer]
+        }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
+    }),
+    new ExtractTextPlugin({
+      filename: 'css/[name].css',
+      disable: false,
+      allChunks: true
+    })
+  ];
+
+
+
+
+  config.plugins.push(
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    }),
+    new CopyWebpackPlugin([{
+      from:'./src'
+    }])
+  )
+
+
+  config.devServer = {
+    contentBase: './src',
+    stats: 'minimal'
+  };
+
+  return config;
+}();
